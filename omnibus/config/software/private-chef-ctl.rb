85000a84ea8febd49f1a930373706a5662f79e0f
Timothy Ehlers <tehlers@expedia.com>
2020-08-10 09:40:34 -0500

    put back in glorious hack-fix

--- omnibus/config/software/private-chef-ctl.rb
+++ omnibus/config/software/private-chef-ctl.rb
@@ -46,4 +46,7 @@ build do
   # These are necessary until we remove all hardcoded references to embedded/bin/*-ctl
   link "#{install_dir}/bin/chef-server-ctl", "#{install_dir}/embedded/bin/chef-server-ctl"
   link "#{install_dir}/bin/chef-server-ctl", "#{install_dir}/embedded/bin/private-chef-ctl"
+ 
+  # TODO: fix this
+  `ln -s /opt/cinc-project /opt/opscode`
 end
