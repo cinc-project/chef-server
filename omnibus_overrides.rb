f0cc35cd3414d42eeec78b876be9de09caff72d0
Timothy Ehlers <tehlers@expedia.com>
2020-08-31 15:39:11 -0500

    added nokogiri pin for faster builds
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -8,5 +8,6 @@ override :chef, version: "v15.17.4"
 override :ohai, version: "v15.12.0"
 override :ruby, version: "2.6.7"
 override :perl, version: "5.18.1"
+override :nokogiri, version: "1.10.10" # if not pinned it forces the entire stack to rebuild
 
 override :openresty, version: "1.19.3.1"
