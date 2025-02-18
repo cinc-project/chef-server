dcd3e14e6fb034db8e05dd2f5fc2450a2ee3ee4f
Lance Albertson <lance@osuosl.org>
2025-02-17 17:30:44 -0800

    fixes
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- src/oc_erchef/apps/chef_telemetry/src/chef_telemetry_worker.erl
+++ src/oc_erchef/apps/chef_telemetry/src/chef_telemetry_worker.erl
@@ -379,7 +379,7 @@ check_send(Hostname) ->
 
 get_fqdn() ->
     error_logger:info_msg("get_fqdn START"),
-    NodeName = envy:get(chef_telemetry, fqdn, <<"">>, string),
+    NodeName = envy:get(chef_telemetry, fqdn, null, string),
     error_logger:info_msg("get_fqdn NodeName: ~p", [NodeName]),
     case NodeName of
         null ->
@@ -387,7 +387,7 @@ get_fqdn() ->
             to_binary("NODE:" ++ binary:bin_to_list(envy:get(oc_chef_wm, actions_fqdn, <<"">>, binary)));
         _ ->
             error_logger:info_msg("get_fqdn NodeName: ~p", [NodeName]),
-            to_binary("NODE:" ++ binary:bin_to_list(erlang:list_to_binary(NodeName)))
+            to_binary("NODE:" ++ binary_to_list(NodeName))
         end.
 
 mask(FQDNs) ->
